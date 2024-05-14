573ac82404da21de975b8a3dffff0928fd61c57c
Rosan Yonghang <roshanlimbu714@gmail.com>
2024-05-14 14:04:56 +0600

    Feature: Add scroll feature to the home screen

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -6,45 +6,49 @@ class Home extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(backgroundColor: Colors.amber, title: const Text('Title')),
       body: Container(
-          height: 800,
           width: double.infinity,
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                width: double.infinity,
-                height: 500,
-                child: Image.network(
-                  "https://cdn.pixabay.com/photo/2024/05/05/05/55/goose-8740266_1280.jpg",
-                  fit: BoxFit.cover,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
                   width: double.infinity,
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 16),
-                child: Text(
-                  'Duck for Sale',
-                  style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Price: Nu.120',
-                    style: TextStyle(fontSize: 32),
+                  height: 1500,
+                  child: Image.network(
+                    "https://cdn.pixabay.com/photo/2024/05/05/05/55/goose-8740266_1280.jpg",
+                    fit: BoxFit.cover,
+                    width: double.infinity,
                   ),
-                  Icon(Icons.shopping_cart)
-                ],
-              ),
-              Container(
-                child: Text(
-                  'This is the description of a duck being sold on the platform',
-                  style: TextStyle(fontSize: 18),
                 ),
-              )
-            ],
+                Container(
+                  margin: EdgeInsets.only(top: 16),
+                  child: Text(
+                    'Duck for Sale',
+                    style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Price: Nu.120',
+                      style: TextStyle(fontSize: 32),
+                    ),
+                    Icon(Icons.shopping_cart)
+                  ],
+                ),
+                Container(
+                  child: Text(
+                    "This is the description of a duck being sold on the platform." +
+                        "This is the description of a duck being sold on the platform. "
+                            "This is the description of a duck being sold on the platform. "
+                            "This is the description of a duck being sold on the platform",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                )
+              ],
+            ),
           )),
     );
   }
